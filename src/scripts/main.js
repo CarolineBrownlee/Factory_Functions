6bc232bc8a6b7e24e384708dcf45d82e5b2256b8
Caroline Brownlee <brownleecaroline@gmail.com>
2019-10-29 10:44:33 -0500

    finished Doctors Exercise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,34 @@
-console.log("This is all working fine and dandy-like.")
+// Lightning Exercise 1: Write a factory function that creates an object that represents a doctor. The function should accept three arguments.
 
-const metals = [
-    { metal: "Sterling Silver", price: 12.42 },
-    { metal: "14K Gold", price: 736.4 },
-    { metal: "24K Gold", price: 1258.9 },
-    { metal: "Platinum", price: 795.45 },
-    { metal: "Palladium", price: 1241.0 }
-]
+const createDoctor = (name, specialty, address) => {
+    return {
+        name: name,
+        specialty: specialty,
+        address: address
+    }
+}
+const doctor = createDoctor("Joseph", "Pediatritian", "Nashville")
+console.log(doctor)
 
+// Lightning Exercise 2: Write a factory function that creates an object that represents a pet. The function should accept two arguments.
+
+const createPet = (name, breed) => {
+    return {
+        name: name,
+        breed: breed
+    }
+}
+
+// Invoke the factory function 3 times and place each animal in an array stored in a variable named BowWowKennels
+
+const BowWowKennels = []
+
+const Briley = createPet("Briley", "Golden Retriever")
+BowWowKennels.push(Briley)
+
+const Atticus = createPet("Atticus", "Tabby Cat")
+BowWowKennels.push(Atticus)
+
+const Trevor = createPet("Trevor", "The Fish")
+BowWowKennels.push(Trevor)
+console.log(BowWowKennels)
