df0d3c8a142157328ea59dded67cd46283b82cdd
Caroline Brownlee <brownleecaroline@gmail.com>
2019-10-29 10:01:37 -0500

    files set up with starter code

---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+console.log("This is all working fine and dandy-like.")
+
+const metals = [
+    { metal: "Sterling Silver", price: 12.42 },
+    { metal: "14K Gold", price: 736.4 },
+    { metal: "24K Gold", price: 1258.9 },
+    { metal: "Platinum", price: 795.45 },
+    { metal: "Palladium", price: 1241.0 }
+]
+
