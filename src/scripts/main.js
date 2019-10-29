c4979c055bdd98d8c32a5be4e40e37c038354b4e
Caroline Brownlee <brownleecaroline@gmail.com>
2019-10-29 11:05:00 -0500

    finished music-row

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,60 @@
 console.log("This is all working fine and dandy-like.")
 
-const metals = [
-    { metal: "Sterling Silver", price: 12.42 },
-    { metal: "14K Gold", price: 736.4 },
-    { metal: "24K Gold", price: 1258.9 },
-    { metal: "Platinum", price: 795.45 },
-    { metal: "Palladium", price: 1241.0 }
-]
+// Your job is to sign each of these promising young music stars to the appropriate label. Create an array for each of these record labels.
+
+// JumpStop Records works with Funk and Rap artists.
+const JumpStopRecords = [] 
+// Chatten Records works with Country and Bluegrass artists.
+const ChattenRecords = []
+// Polar Records works with Pop artists.
+const PolarRecords = []
+
+// Create a factory function for each possible genre (e.g. createBluegrassArtist()). Then invoke the appropriate function for each of the following artists and place the resulting object in the corresponding label array.
+
+const createFunkAndRapArtist = (name, age) => {
+    return {    
+        name: name,
+        age: age
+    }
+}
+
+const createCountryAndBluegrassArtist = (name, age) => {
+    return {    
+        name: name,
+        age: age
+    }   
+}
+
+const createPopArtist = (name, age) => {
+    return {    
+        name: name,
+        age: age
+    }
+}
+
+const Bruce = createCountryAndBluegrassArtist("Brue Atkins", 23)
+ChattenRecords.push(Bruce)
+
+const Jensen = createFunkAndRapArtist("Jensen Brown", 20)
+JumpStopRecords.push(Jensen)
+
+const Dre = createFunkAndRapArtist("Dre Funkz", 21)
+JumpStopRecords.push(Dre)
+
+const Bart = createCountryAndBluegrassArtist("Bartholomew Danielson", 23)
+ChattenRecords.push(Bart)
+
+const Avilee = createCountryAndBluegrassArtist("Avilee Dallas", 19) 
+ChattenRecords.push(Avilee)
+
+const Austin = createPopArtist("Austin Kinkaid", 22)
+PolarRecords.push(Austin)
+
+const Loyoncé = createFunkAndRapArtist("Loyoncé Branis", 27)
+JumpStopRecords.push(Loyoncé)
+
+console.table("Polar Records", PolarRecords)
+console.table("Jump Stop Records", JumpStopRecords)
+console.table("Chatten Records", ChattenRecords)
+
 
